6aa901a406072dae6f2362af032701810f87fdee
FilipeAdmin <filipeadmin@Filipe-MacBook.local>
2021-01-23 20:01:26 -0300

    create a first test

--- test/todo.test.js
+++ test/todo.test.js
@@ -1,6 +1,20 @@
 const { describe,it,before} = require ('mocha')
 const { expect } = require('chai')
+const Todo = require("../src/todo")
 
 describe('todo', ()=>{
-    it('test')
+    describe('#isValid', ()=>{
+        it('should return invalid when creating without text',()=>{
+            const data = {
+                text : '',
+                when : new Date("2020-12-01")
+            }
+            const todo = new Todo(data)
+            const result = todo.isValid()
+            expect(result).to.be.not.ok
+
+        })
+        it('should return invalid when creating without when or when is invalid')
+        it('should have "id","text","when" and "status" properties after creating object')
+    })
 })
